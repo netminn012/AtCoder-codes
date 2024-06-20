f61f52fce5fb338b0a50d117587a565bfe16683c
どっかのネット民 <115701341+netminn012@users.noreply.github.com>
2024-06-20 05:20:50 +0000

    ABC206やった

--- コンテスト練習/abc/abc206/a/mani.py
+++ コンテスト練習/abc/abc206/a/mani.py
@@ -1,3 +1,9 @@
-a,b = map(int, input().split())
-# 100mlあたりのカロリーがakaclの飲み物をbml飲んだ時のカロリーを計算
-print(a*b/100)
+import math
+n = int(input())
+n = math.floor(n * 1.08)
+if n < 206:
+    print('Yay!')
+elif n == 206:
+    print('so-so')
+else:
+    print(':(')
